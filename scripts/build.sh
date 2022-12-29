8b514c0c7602efcc5bf465a5c0cc387db3d62afb
Alex M <alex@alexos.dev>
2022-12-29 22:42:56 +0000

    Fix cloud-run build stage

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,27 @@ set -euoE pipefail
 
 pushd "$(dirname "${BASH_SOURCE[0]}")/../" >/dev/null
 
+
+if [[ ${CI_SERVER:-} == "yes" ]]; then
+  wget --no-verbose -O hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.90.1/hugo_extended_0.90.1_Linux-64bit.tar.gz && \
+  tar zxf hugo.tar.gz && \
+  mv ./hugo /usr/local/bin/ && \
+  rm hugo.tar.gz
+fi
+
+mkdir -p "www/"
+
+pushd "www/" > /dev/null
+rm -rf ./*
+popd >/dev/null
+
+pushd "src/" >/dev/null
+hugo --source .
+popd >/dev/null
+
+
+pushd "$(dirname "${BASH_SOURCE[0]}")/../" >/dev/null
+
 image_name="eu.gcr.io/${PROJECT_ID}/${SERVICE}"
 
 docker pull "${image_name}":latest || true
