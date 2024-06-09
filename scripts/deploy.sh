3bde0a6ba69a412843189dd4ee7345cd36b86bd1
Alex M <alex@alexos.dev>
2024-06-09 20:54:06 +0100

    Set to actually deploy to CloudRun

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ IMAGE_TAG=${IMAGE_NAME}:${CI_COMMIT_SHA}-$(echo "${CI_COMMIT_TIMESTAMP}" | sed '
 pushd "$(dirname "${BASH_SOURCE[0]}")/../terraform/" >/dev/null
 
 terraform init -backend-config=bucket="${GCP_PROJECT_ID}"-apps-tfstate -backend-config=prefix=alexmoss-co-uk
-terraform plan -var gcp_project_id="${GCP_PROJECT_ID}" \
+terraform apply -auto-approve -var gcp_project_id="${GCP_PROJECT_ID}" \
   -var app_name="${APP_NAME}" \
   -var image_tag="${IMAGE_TAG}" \
   -var region="${REGION}"
