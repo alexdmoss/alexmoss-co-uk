081f73b64bc88fc7738930133892aba3efc47672
Alex M <alex@alexos.dev>
2024-06-19 15:30:44 +0100

    Force override of domain mapping

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,11 +56,13 @@ resource "google_cloud_run_v2_service_iam_member" "allow-public-acess" {
 resource "google_cloud_run_domain_mapping" "apex-domain" {
   name     = var.domain
   location = google_cloud_run_v2_service.app.location
+
   metadata {
     namespace = var.gcp_project_id
   }
   spec {
     route_name = google_cloud_run_v2_service.app.name
+    force_override = true
   }
 }
 
